42d41a902ea0611496fc0b759ed7c72e98ecdfaf
Pratham3642 <prathameshmavalkar34@gmail.com>
2025-12-09 11:26:40 +0530

    first commit

--- C_programming/Classwork/Hello.c
+++ C_programming/Classwork/Hello.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
 //write a program to check whether a number is even or odd
-// void main(){
-//     int x=3;
-//     if(x%2==0){
-//         printf("Even Number");
-//     }
-//     else{
-//         printf("Odd Number");
-//     }
-// }
+void main(){
+    int x=3;
+    if(x%2==0){
+        printf("Even Number");
+    }
+    else{
+        printf("Odd Number");
+    }
+}
 
 //Check whether person is eligible to vote or not
-// void main(){
-//     int age = 19;
-//     if(age>=18){
-//         printf("Eligible to vote");
-//     }
-//     else{
-//         printf("Not Eligible to vote");
-//     }
-// }
+void main(){
+    int age = 19;
+    if(age>=18){
+        printf("Eligible to vote");
+    }
+    else{
+        printf("Not Eligible to vote");
+    }
+}
 
 //Check whether a number is positive or negative
-// void main(){
-//     int num = -4;
-//     if(num>=0){
-//         printf("Positive Number");
-//     }
-//     else{
-//         printf("Negative Number");
-//     }
-// }
+void main(){
+    int num = -4;
+    if(num>=0){
+        printf("Positive Number");
+    }
+    else{
+        printf("Negative Number");
+    }
+}
 
 //Check whether a year is leap year or not
 void main(){
